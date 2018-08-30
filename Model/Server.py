e925b08ac6191be4808c8f8ea31328054b6f9b03
Abhishek Singh <abhishek.singh@ebizontek.com>
2018-08-30 11:59:51 +0530

    fixed

--- Model/Server.py
+++ Model/Server.py
@@ -1,28 +1,44 @@
-from sqlalchemy import Column, Integer, Text, create_engine
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+import datetime as dt
+from datetime import datetime
 
 Base = declarative_base()
 
 
-class Server(Base):
+class Instances(Base):
     """ This Class Store Detail of the Server"""
-    __tablename__ = "Server"
+    __tablename__ = "Instances"
     id = Column(Integer, autoincrement=True, primary_key=True)
-    ip = Column(Text, nullable=False)
-    url = Column(Text, nullable=False)
-    dbUser = Column(Text, nullable=False)
-    dbName = Column(Text, nullable=False)
-    dbPass = Column(Text, nullable=False)
-    sshUser = Column(Text, nullable=False)
-    sshPass = Column(Text, nullable=False)
+    ip = Column(String(100), nullable=False)
+    url = Column(String(100), nullable=False)
+    dbUser = Column(String(100), nullable=False)
+    dbName = Column(String(100), nullable=False)
+    dbPass = Column(String(100), nullable=False)
+    sshUser = Column(String(100), nullable=False)
+    sshPass = Column(String(100), nullable=False)
 
 
 class User(Base):
     """ This Class Store the Detail of the User"""
     __tablename__ = "User"
     id = Column(Integer, autoincrement=True, primary_key=True)
-    email = Column(Text, nullable=False)
+    role = Column(String(100), nullable=True)
+    email = Column(String(100), unique=True, nullable=False)
+    name = Column(String(100), nullable=True)
+    avatar = Column(String(200))
+    active = Column(Boolean, default=False)
+    tokens = Column(String(200))
+    created_at = Column(DateTime, default=dt.datetime.utcnow())
 
 
-engine = create_engine('mysql+pymysql://root:root@localhost:3289/automation')
+class Roles(Base):
+    """ This Class Store the Detail of the User"""
+    __tablename__ = "Roles"
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    role = Column(String(100), nullable=True)
+    capabilities = Column(String(200), nullable=True)
+
+
+engine = create_engine('mysql+pymysql://root:root@localhost:3306/automation')
 Base.metadata.create_all(engine)
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from Model.Server import User, Server
+from Model.Server import User, Instances
 
 
 app = Flask(__name__)
@@ -10,9 +10,10 @@ def home():
 	return render_template("home.html")
 
 
-@app.route("/register")
+@app.route("/login")
 def register():
-	return render_template("register.html")
+	return render_template("login.html")
+
 
 @app.route("/admin")
 def admin():
